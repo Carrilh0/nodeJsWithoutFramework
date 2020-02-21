2cf806de8559d5b88a7faf5aceeed9ada9a7daa4
Vitor Carrilho <vitor.carrilho@hotmail.com>
2020-02-21 01:51:24 -0300

    controller de rotas e ações

---
+++ controller.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const url = require('url');
+
+module.exports = http.createServer((req, res) => {
+
+    var service = require('./service.js');
+    const reqUrl = url.parse(req.url, true);
+
+    // GET Endpoint
+    if (reqUrl.pathname == '/sample' && req.method === 'GET') {
+        console.log('Request Type:' +
+            req.method + ' Endpoint: ' +
+            reqUrl.pathname);
+
+        service.sampleRequest(req, res);
+    }else{
+        service.end('HOME');
+    }
+});
